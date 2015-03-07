cdede2afa96ac6fd524e52736809247452174523
Yoshua Wuyts <i@yoshuawuyts.com>
2015-03-07 04:05:51 +0100

    writes: fix

--- bin/meta.js
+++ bin/meta.js
@@ -2,43 +2,46 @@ const assert = require('assert')
 const exec = require('child_process').exec
 
 module.exports = makeConfig
-makeConfig.getConfig = getConfig
-makeConfig.writeFiles = writeFiles
 
-// create configuration
+// create confuration
 // @param {Object}
 // @param {Function}
-function makeConfig (obj, cb) {
-  obj = obj
-  cb(obj)
+function makeConfig (conf, cb) {
+  conf = getConfig(conf)
+  whoami(conf, function (name) {
+    conf.name = name
+    conf.username = name
+    conf.meta.name = name
+    cb(conf)
+  })
 }
 
-/**
- * Get configuration.
- */
-function getConfig (config) {
+ // get confuration.
+ // @param {Object} conf
+function getConfig (conf) {
   var date = new Date()
   var day = date.getDate()
   var month = date.getMonth()
 
-  config.year = date.getFullYear()
-  config.day = day.toString().length === 2 ? day : '0' + day
-  config.month = month.toString().length === 2 ? month : '0' + month
+  conf.year = date.getFullYear()
+  conf.day = day.toString().length === 2 ? day : '0' + day
+  conf.month = month.toString().length === 2 ? month : '0' + month
 
-  console.log('  packageName: ' + config.packageName)
+  console.log('  packageName: ' + conf.meta.packageName)
 
-  config.date = config.year + '-' + config.month + '-' + config.day
-  console.log('  date: ' + config.date)
+  conf.date = conf.year + '-' + conf.month + '-' + conf.day
+  console.log('  date: ' + conf.date)
+  return conf
 }
 
-function writeFiles (program) {
-  console.log('')
-  console.log('configuration')
-  var config = {}
-
+// find out what the username is
+// @param {Object} conf
+// @param {Function} cb
+function whoami (conf, cb) {
   exec('npm whoami', function (err, name) {
     assert.ifError(err)
-    config.name = name.replace(/(\n)/gm, '')
-    console.log('  user: ' + config.name)
+    name = name.replace(/(\n)/gm, '')
+    console.log('  user: ' + name)
+    cb(name)
   })
 }
